b3d98cac823f2516e94f6ed114b2c2c0a14ee9c2
enzo.bras <enzo.bras@etu.univ-orleans.fr>
2023-10-04 09:11:46 +0200

    creation super user + ajout du modele jeux dans admin

--- djangoGaming/lesJeux/admin.py
+++ djangoGaming/lesJeux/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from lesJeux.models import Jeux
 
-# Register your models here.
+
+admin.site.register(Jeux)
+
+class JeuxAdmin(admin.ModelAdmin):
+    list_display=('nom', 'description', 'studio', 'tags', 'date')
+
+admin.register(Jeux, JeuxAdmin)
