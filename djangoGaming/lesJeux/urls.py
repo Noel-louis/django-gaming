00fe606f2c9ecbfabab69f998bf88a521bceff0c
Lnoel <louisnoel45000@gmail.com>
2023-10-04 09:25:46 +0200

    ajout page affichage jeu unique

--- djangoGaming/lesJeux/urls.py
+++ djangoGaming/lesJeux/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('jeu/<str:name>', views.jeu, name='jeu'),
 ]
--- djangoGaming/lesJeux/views.py
+++ djangoGaming/lesJeux/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-  return HttpResponse('bonjour à tous et bienvenue sur django gaming !')
+  return render(request, template_name='base.html')
+
+def jeu(request, name):
+  jeu = {
+    'nom': name,
+    'description': 'Un jeu de combat',
+    'date': '2021-01-01',
+    'studio': 'Studio 1',
+    'tags': 'Combat, 2D, 3D',
+  }
+  return render(request, template_name='jeu.html', context={'jeu': jeu})
