5ffe62f7f72884eaac2b56c529bbf7a3c51779c0
enzo.bras <enzo.bras@etu.univ-orleans.fr>
2023-10-10 14:28:55 +0200

    mise en relation modele/template

--- djangoGaming/lesJeux/views.py
+++ djangoGaming/lesJeux/views.py
@@ -1,62 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from lesJeux.models import Jeux
 
 def home(request):
   return render(request, template_name='base.html')
 
 def jeu(request, name):
-  jeu = {
-    'nom': name,
-    'description': 'Un jeu de combat',
-    'date': '2021-01-01',
-    'studio': 'Studio 1',
-    'tags': 'Combat, 2D, 3D',
-  }
+  jeu = Jeux.objects.get(nom=name)
   return render(request, template_name='jeu.html', context={'jeu': jeu})
 
 def listejeux(request):
-  jeux = [
-    {
-      'nom': 'Jeu 1',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-    {
-      'nom': 'Jeu 2',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-    {
-      'nom': 'Jeu 3',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-    {
-      'nom': 'Jeu 4',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-    {
-      'nom': 'Jeu 5',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-    {
-      'nom': 'Jeu 6',
-      'description': 'Un jeu de combat',
-      'date': '2021-01-01',
-      'studio': 'Studio 1',
-      'tags': 'Combat, 2D, 3D',
-    },
-  ]
+  jeux = Jeux.objects.all()
   return render(request, template_name='listejeux.html', context={'jeux': jeux})
