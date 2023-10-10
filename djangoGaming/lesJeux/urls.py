ece6bd0fb5df8b70c9785614c1f11c223e6cf644
Lnoel <louisnoel45000@gmail.com>
2023-10-10 14:18:07 +0200

    ajout de la liste des jeux lien href entre la liste et les jeux.

--- djangoGaming/lesJeux/urls.py
+++ djangoGaming/lesJeux/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('jeu/<str:name>', views.jeu, name='jeu'),
+    path('listejeux', views.listejeux, name='listejeux'),
 ]
--- djangoGaming/lesJeux/views.py
+++ djangoGaming/lesJeux/views.py
@@ -13,3 +13,50 @@ def jeu(request, name):
     'tags': 'Combat, 2D, 3D',
   }
   return render(request, template_name='jeu.html', context={'jeu': jeu})
+
+def listejeux(request):
+  jeux = [
+    {
+      'nom': 'Jeu 1',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+    {
+      'nom': 'Jeu 2',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+    {
+      'nom': 'Jeu 3',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+    {
+      'nom': 'Jeu 4',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+    {
+      'nom': 'Jeu 5',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+    {
+      'nom': 'Jeu 6',
+      'description': 'Un jeu de combat',
+      'date': '2021-01-01',
+      'studio': 'Studio 1',
+      'tags': 'Combat, 2D, 3D',
+    },
+  ]
+  return render(request, template_name='listejeux.html', context={'jeux': jeux})
