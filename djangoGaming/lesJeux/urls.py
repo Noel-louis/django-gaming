984f39697e93a77c16f898422375e86c495ed573
Lnoel <louisnoel45000@gmail.com>
2023-10-11 09:55:17 +0200

    ajout de studios et des pages correspondantes

--- djangoGaming/lesJeux/urls.py
+++ djangoGaming/lesJeux/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('creer/', views.creer_jeu, name='creerjeux'),
     path('modifier/<str:nom>/', views.modifier_jeu, name='modifierjeux'),
     path('supprimer/<str:nom>/', views.supprimer_jeu, name='supprimerjeux'),
+    path('studio/<str:name>', views.studio, name='studio'),
+    path('listestudios', views.listestudios, name='listestudios'),
+    path('creerstudio/', views.creer_studio, name='creerstudio'),
+    path('modifierstudio/<str:nom>/', views.modifier_studio, name='modifierstudio'),
+    path('supprimerstudio/<str:nom>/', views.supprimer_studio, name='supprimerstudio'),
 ]
--- djangoGaming/lesJeux/views.py
+++ djangoGaming/lesJeux/views.py
@@ -39,3 +39,37 @@ def supprimer_jeu(request, nom):
     jeu = get_object_or_404(Jeux, nom=nom)
     jeu.delete()
     return redirect('listejeux')
+
+def studio(request, name):
+  studio = Studio.objects.get(nom=name)
+  return render(request, template_name='studio.html', context={'studio': studio})
+
+def listestudios(request):
+  studios = Studio.objects.all()
+  return render(request, template_name='listestudios.html', context={'studios': studios})
+
+def creer_studio(request):
+    if request.method == 'POST':
+        form = StudioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listestudios')
+    else:
+        form = StudioForm()
+    return render(request, 'creerstudio.html', {'form': form})
+
+def modifier_studio(request, nom):
+    studio = get_object_or_404(Studio, nom=nom)
+    if request.method == 'POST':
+        form = StudioForm(request.POST, instance=studio)
+        if form.is_valid():
+            form.save()
+            return redirect('listestudios')
+    else:
+        form = StudioForm(instance=jeu)
+    return render(request, 'modifierstudios.html', {'form': form})
+
+def supprimer_jeu(request, nom):
+    studio = get_object_or_404(Studio, nom=nom)
+    studio.delete()
+    return redirect('listejeux')
