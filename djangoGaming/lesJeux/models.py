f312664e4ebebb3d38346cf7bd084876789a6ae8
enzo.bras <enzo.bras@etu.univ-orleans.fr>
2023-10-13 15:30:01 +0200

    ajout du model - bug a résoudre

--- djangoGaming/lesJeux/models.py
+++ djangoGaming/lesJeux/models.py
@@ -9,3 +9,12 @@ class Jeux(models.Model):
 
     def __str__(self):
         return self.nom
+    
+class Studio(models.Model):
+    nom = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+    date_creation = models.DateField()
+    jeux_cree = models.ForeignKey(Jeux, on_delete=models.CASCADE, null=True, related_name="jeux_cree")
+
+    def __str__(self):
+        return self.nom
