40f540ca529f1cc060ac1b7940243059265bbb6b
Lnoel <louisnoel45000@gmail.com>
2023-10-17 17:37:03 +0200

    ajout des tags dans le modele

--- djangoGaming/lesJeux/models.py
+++ djangoGaming/lesJeux/models.py
@@ -4,7 +4,7 @@ class Jeux(models.Model):
     nom = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
     studio = models.ForeignKey('Studio', on_delete=models.CASCADE, null=True, related_name="studio")
-    tags = models.CharField(max_length=250)
+    tags = models.ManyToManyField('Tag', related_name="tags")
     date = models.DateField()
 
     def __str__(self):
@@ -14,6 +14,13 @@ class Studio(models.Model):
     nom = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
     date_creation = models.DateField()
+
+    def __str__(self):
+        return self.nom
+    
+class Tag(models.Model):
+    nom = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
 
     def __str__(self):
         return self.nom
